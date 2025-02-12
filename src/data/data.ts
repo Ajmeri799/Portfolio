f3a6c3fe8506ccbc519758af8e5f84c456af3b0f
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2025-02-13 03:46:00 +0530

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,68 +1,77 @@
 export const techStacks = [
   {
     icon: "/techstack/HTML.svg",
+    alt: "html",
     techname: "HTML5",
-  },
-    {
-    icon: "/social/github.svg",
-    alt: "github",
-    link: "https://github.com/ajmeri",
   },
   {
     icon: "/techstack/CSS3.svg",
+    alt: "css",
     techname: "CSS3",
   },
   {
     icon: "/techstack/JS.svg",
+    alt: "js",
     techname: "JavaScript",
   },
   {
     icon: "techstack/TS.svg",
+    alt: "ts",
     techname: "TypeScript",
   },
   {
     icon: "/techstack/Java.svg",
+    alt: "java",
     techname: "JAVA",
   },
 
   {
     icon: "/techstack/React.svg",
+    alt: "react",
     techname: "ReactJS",
   },
   {
     icon: "/techstack/next.svg",
+    alt: "nextjs",
     techname: "NextJS",
   },
 
   {
     icon: "/techstack/Tailwind.svg",
+    alt: "Tailwind css",
     techname: "Tailwind css",
   },
   {
     icon: "/techstack/Redux.svg",
+    alt: "redux",
     techname: "Redux",
   },
 
   {
     icon: "/techstack/aceternity.webp",
+    alt: "aui",
     techname: "Aceternity  UI",
   },
   {
     icon: "/techstack/node.svg",
+    alt: "nodejs",
     techname: "NodeJs",
   },
   {
     icon: "https://appwrite.io/images/logos/appwrite.svg",
+    alt: "appwrite",
     techname: "Appwrite",
   },
 
   {
     icon: "/techstack/vercel.svg",
+    alt: "vercel",
     techname: "Vercel",
   },
 
   {
     icon: "/techstack/Github.svg",
+    alt: "github",
     techname: "Github",
   },
 ];
@@ -70,16 +79,16 @@ export const techStacks = [
 export const Education = [
   {
     id: 1,
-    degree: "Master’s Degree",
-    des: "Pursuing my Master's of Computer Application from RGPV University, Bhopal",
+    degree: "Bachelor’s Degree",
+    des: "Pursuing my Btech in Computer Science & Engineering from United College of Engineering & Research, Naini Prayagraj",
     thumbnail: "/images/images.png",
-    yearOfcompletion: "2023-2025",
+    yearOfcompletion: "2023-2026",
     completed: "Pursuing",
   },
   {
     id: 2,
-    degree: "Bachelor’s Degree",
-    des: "Completed my Bachelor's of Computer Application from u, Prayagraj",
+    degree: "Diploma",
+    des: "Completed my Diploma in Computer Science & Engineering  from Government Polytechnic Jaunpur, Jaunpur",
     thumbnail: "/images/6027.png_860.png",
     yearOfcompletion: "2020-2023",
     completed: "completed",
@@ -87,25 +96,51 @@ export const Education = [
 ];
 // Social icons
 
-export const sociallist = [
+export const social = [
   {
     icon: "/social/github.svg",
     alt: "github",
-    link: "https://github.com/sakshi936",
-  },
-  {
-    icon: "/social/discord.svg",
-    alt: "discord",
-    link: "https://discord.com/channels/sakshi261_21846",
-  },
-  {
-    icon: "/social/linkedin.svg",
-    alt: "linkedin",
-    link: "https://www.linkedin.com/in/sakshi6392/",
+    link: "https://github.com/ajmeri799",
   },
   {
     icon: "/social/mail.svg",
     alt: "emain",
-    link: "mailto: sakshipandey2601@gmail.com",
+    link: "mailto:ajmeriguniya@gmail.com",
+  },
+  {
+    icon: "/social/linkedin.svg",
+    alt: "linkedin",
+    link: "https://www.linkedin.com/in/ajmeri799guniya/",
+  },
+];
+
+// projects data
+export const projects = [
+  {
+    name: "Authentication system",
+    quote:
+      "This Next application demonstrates user authentication using Appwrite with Nextjs and TypeScript. ",
+    src: "/project/login.svg",
+    alt: "auth",
+    designation: "https://github.com/Ajmeri799/Next_Auth",
+    code: "source code",
+  },
+  {
+    name: "To Do List Application",
+    quote:
+      "Created with React and Context API for efficient state management. ",
+    src: "/project/todo.svg",
+    alt: "todo",
+    designation: "https://github.com/sakshi936/ToDo",
+    code: "source code",
+  },
+  {
+    name: "Space Music Academy",
+    quote:
+      "A Next.js project of A Music Academy website using TypeScript, Tailwind and Aceternity  ui for user iterface",
+    src: "/project/download.png",
+    alt: "music",
+    designation: "https://github.com/Ajmeri799/Music_Courses",
+    code: "source code",
   },
 ];
